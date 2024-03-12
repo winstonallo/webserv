af4ef5f486fafa3700cf6547bb5a5ba230aa6f4d
winstonallo <arthurbiedchar@gmail.com>
2024-03-12 21:43:05 +0100

    rm: forward declarations

--- inc/Headers.hpp
+++ inc/Headers.hpp
@@ -30,13 +30,16 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-class Director;
-class ServerInfo;
-class ClientInfo;
-class Config;
+#include "Node.hpp"
+#include "ServerInfo.hpp"
+#include "ClientInfo.hpp"
+#include "Config.hpp"
+#include "Log.hpp"
+#include "Utils.hpp"
+#include "LocationInfo.hpp"
+#include "Director.hpp"
 
 #define EXPECTED_EXT ".conf"
-
 #define UNINITIALIZED_SCOPE "uninitialized scope: expected identifier"             
 #define UNEXPECTED_NL "unexpected newline - terminate value lines with ';'"
 #define UNTERM_VALUE_SCOPE "unterminated value scope - terminate value lines with ';'"
@@ -47,31 +50,12 @@ class Config;
 #define INVALID_EXT "invalid file extension (expected: '.conf')"
 #define INV_HEADER "invalid config file header (expected: 'webserv')"
 #define UNCLOSED_QUOTE "unclosed quote sequence"
-
 #define NOT_FOUND "could not open file"
-
-
 #define FALLBACK "falling back to default config: '\033[1mconfig/webserv.conf\033[0m'"
-
 #define RULES "refer to '\033[1mconfig/rules/config-formatting-rules.md\033[0m' for more details"
-
-#include "Node.hpp"
-#include "ServerInfo.hpp"
-#include "ClientInfo.hpp"
-#include "Config.hpp"
-#include "Log.hpp"
-#include "Utils.hpp"
-#include "LocationInfo.hpp"
-#include "Director.hpp"
-
-#define EXIT_FAILURE 1
-#define EXIT_SUCCESS 0
 
 #define BOLD "\033[1m"
 #define RESET "\033[0m"
 #define UNDERLINE "\033[4m"
-
-
-
 
 #endif
