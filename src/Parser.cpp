c8661841bc1e62e0ba21166b83e119cb615af52a
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-03-12 12:17:42 +0000

    add: quoted words in config file

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -44,15 +44,29 @@ std::vector <std::string>	Parser::split(const std::string& str, const std::strin
 	return ret;
 }
 
+// splits line based on delimiters, storing quoted sequences as one word regardless of delimiters
+//
+// @param 	str: string to split
+// @param 	delimiters: string containing all delimiters
+//
+// @return	vector of strings
 std::vector <std::string>	Parser::split_keep_quoted_words(const std::string& str, const std::string& delimiters)
 {
 	bool						in_quotes = false;
 	size_t						left = 0, right = 0;
 	std::vector <std::string>	ret;
 
-	while (right < str.size())
+	while (right <= str.size())
 	{
-		if (str[right] == '"' && (right == 0 || str[right - 1] != '\\'))
+		if (right == str.size() || (in_quotes == false && delimiters.find(str[right]) != std::string::npos)) 
+		{
+                if (left != right) 
+				{
+                    ret.push_back(str.substr(left, right - left));
+                }
+                left = right + 1;
+		}
+		else if (str[right] == '"' && (right == 0 || str[right - 1] != '\\'))
 		{
 			in_quotes = !in_quotes;
 			if (in_quotes == false)
@@ -60,18 +74,6 @@ std::vector <std::string>	Parser::split_keep_quoted_words(const std::string& str
 				ret.push_back(str.substr(left + 1, right - left - 1));
 				left = right + 1;
 			}
-			else
-			{
-				left = right;
-			}
-		}
-		else if (in_quotes == false && delimiters.find(str[right]) != std::string::npos)
-		{
-			if (left != right)
-			{
-				ret.push_back(str.substr(left, right - left));
-			}
-			left = right + 1;
 		}
 		right++;
 	}
@@ -89,42 +91,45 @@ std::string		Parser::trim_comment(const std::string& str, const std::string& del
 
 std::vector <std::pair <std::string, int> > Parser::split_keep_delimiters(const std::string& str, const std::string& delimiters)
 {
-	size_t 			left = 0, right;
-	std::vector		<std::pair<std::string, int> > ret;
-	int line_number = 1;
+	int 										line_number = 1;
+	bool										in_quotes = false;
+	size_t 										left = 0, right = 0;
+	std::vector	<std::pair<std::string, int> > 	ret;
 
-	while (left < str.size()) 
+	while (right <= str.size()) 
 	{
-		right = str.find_first_of(delimiters, left);
-
-		if (right != std::string::npos) 
+		if (str[right] == '"' && (right == 0 || str[right - 1] != '\\'))
 		{
-			std::string segment = str.substr(left, right - left), delimiter(1, str[right]);
-
-			if (segment.empty() == false)
-			{
-				std::string content = Parser::trim(segment, "\t\n ");
-				if (!content.empty())
-					ret.push_back(std::make_pair(content, line_number));
-			}
-			line_number += std::count(segment.begin(), segment.end(), '\n');
-			if (delimiter.empty() == false) 
-			{
-				ret.push_back(std::make_pair(delimiter, line_number));
-			}
-			left = right + 1;
-		} 
-		else 
-		{
-			std::string remaining = str.substr(left), content = Parser::trim(remaining, "\t\n ");
-
-			line_number += std::count(remaining.begin(), remaining.end(), '\n');
-			if (content.empty() == false)
-			{
-				ret.push_back(std::make_pair(content, line_number));
-			}
-			break;
+			in_quotes = !in_quotes;
 		}
+		else if (in_quotes == false && (right == str.size() || delimiters.find(str[right]) != std::string::npos))
+		{
+			if (left < right)
+			{
+				std::string segment = Parser::trim(str.substr(left, right - left), "\t\n ");
+				if (segment != "")
+				{
+					ret.push_back(std::make_pair(segment, line_number));
+				}
+				line_number += std::count(segment.begin(), segment.end(), '\n');
+			}
+			if (right < str.size())
+			{
+				std::string delimiter(1, str[right]);
+				if (delimiter != " ")
+				{
+					ret.push_back(std::make_pair(delimiter, line_number));
+				}
+			}	
+			left = right + 1;
+		}
+		if (in_quotes == true && right == str.size() - 1)
+		{
+			std::string segment = Parser::trim(str.substr(left, right - left), "\t\n ");
+			ret.push_back(std::make_pair(Parser::trim(segment, "\t\n "), line_number));
+			line_number += std::count(segment.begin(), segment.end(), '\n');
+		}
+		right++;
 	}
 	return ret;
 }
