ca6bcda161a0881e36fdce30f82804837438a8ac
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-03-20 08:27:09 +0000

    adapted the handling of values in the server map for safe access

--- inc/Config.hpp
+++ inc/Config.hpp
@@ -6,6 +6,8 @@
 #include <netinet/in.h>
 #include <string>
 
+typedef std::map <std::string, std::vector <std::string> > server_map;
+
 class Config
 {
 	public:
@@ -20,11 +22,11 @@ class Config
 		void									set_routes(std::map <std::string, std::map <std::string, std::vector <std::string> > >& raw_routes);
 
 
-		void									handle_port(const int port, ServerInfo* new_server);
-		void									handle_server_names(const std::vector <std::string>& server_names, ServerInfo* new_server);
-		void									handle_host(std::string& hostname, ServerInfo* new_server);
-		std::string								handle_access_log(std::map <std::string, std::vector <std::string> >& server);
-		int										handle_client_max_body_size(const std::string& client_max_body_size);
+		void									handle_port(server_map& server, ServerInfo* new_server);
+		void									handle_server_names(server_map& server, ServerInfo* new_server);
+		void									handle_host(server_map& server, ServerInfo* new_server);
+		void									handle_access_log(server_map& server, ServerInfo* new_server);
+		void									handle_client_max_body_size(server_map& server, ServerInfo* new_server);
 
 		std::string                             generate_default_error_page(const int status_code);
 
@@ -47,6 +49,7 @@ class Config
 };
 
 #define CLIENT_MAX_BODY_SIZE_DEFAULT 1000000
+#define CLIENT_MAX_BODY_SIZE_MAX 10000000
 #define ACCESS_LOG_DEFAULT "access.log"
 #define DEFAULT_ERROR_PAGE "files/error_pages/400.html"
 
