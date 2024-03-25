eec9cd767560c1a809d6f5af8e3f4cb8af05416c
ARTHUR BIED-CHARRETON <arthurbiedchar@gmail.com>
2024-03-25 20:30:35 +0100

    add: random things, im just procrastinating at this point

--- inc/CGI.hpp
+++ inc/CGI.hpp
@@ -9,17 +9,22 @@ class CGI
 {
     public:
 
+        std::string                             get_cmd();
+        void                                    set_arguments(const std::string& args);
+        std::string                             execute_cgi();
+
         CGI(const std::map<std::string, std::string>& env, const std::string& body, LocationInfo* location);
         ~CGI();
-    
+
     private:
 
-        LocationInfo*                           _location;
-        std::string                             _response_body;
-        std::string                             _request_body;
-        std::map <std::string, std::string>     _env_map;
+        std::string                             _cmd;
         char**                                  _env;
-
+        LocationInfo*                           _location;
+        char**                                  _arguments;
+        std::string                             _request_body;
+        std::string                             _response_body;
+        std::map <std::string, std::string>     _environment_map;
 };
 
 #endif
--- src/CGI.cpp
+++ src/CGI.cpp
@@ -1,23 +1,32 @@
 #include "CGI.hpp"
 #include <cstring>
-#include <stdexcept>
-#include <iostream>
 
-CGI::CGI(const std::map<std::string, std::string>& env, const std::string& body, LocationInfo* location)
+CGI::CGI(const std::map<std::string, std::string>& env_map, const std::string& body, LocationInfo* location)
 {
-    _env_map = env;
+    _environment_map = env_map;
     _request_body = body;
     _location = location;
-    _env = new char*[_env_map.size() + 1];
-    _env[_env_map.size()] = NULL;
+    _env = new char*[_environment_map.size() + 1];
+    _env[_environment_map.size()] = NULL;
     int i = 0;
-    for (std::map <std::string, std::string>::iterator it = _env_map.begin(); it != _env_map.end(); it++)
+
+    for (std::map <std::string, std::string>::iterator it = _environment_map.begin(); it != _environment_map.end(); it++)
     {
         _env[i++] = strdup((it->first + "=" + it->second).c_str());
     }
 }
 
-CGI::~CGI() 
+std::string    CGI::get_cmd()
+{
+    return ""; // TODO: implement this
+}
+
+void    CGI::set_arguments(const std::string& args)
+{
+    (void)args; // TODO: implement this
+}
+
+CGI::~CGI()
 {
     for (int i = 0; _env[i]; i++)
     {
