f05eb714a2f225648b29538c42b94b28f582458b
winstonallo <arthurbiedchar@gmail.com>
2025-01-21 00:28:21 +0100

    add: README.md

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
 # webserv
-HTTP/1.1 compliant webserver in C++98.
+This project's aim is to build a webserver adhering to the HTTP/1.1 protocol. 
+
+## How to Run
+
+### Requirements
+* `make`
+* `C++98`
+
+### Execution
+```sh
+git clone git@github.com:winstonallo/webserv.git
+cd webserv
+make
+./webserv
+```
+The logs printed to the console will show you which URL to look up in order to look at the static website we programmed for testing purposes.
+
+The [default configuration](assets/config/webserv.conf) shows many of the implemented features.
+#### Links
+Once executed **with the default configuration**, follow these links to test functionality:
+* [CGI (Python)](http://localhost:8080/cgi-bin/date.py)
+* [CGI (Bash)](http://localhost:8080/cgi-bin/do_shell.sh)
+* [File upload and deletion](http://localhost:8080/tests.html)
+* [Session management](http://localhost:8080/cgi-bin/session.py)
+
+## Requirements
+`webserv` must:
+* Support non-blocking I/O operations for up to 1024 concurrent clients using only one `select` call, which must check for reads and writes at the same time.
+* Support client timeout in case oh hanging requests.
+* Have accurate response status codes.
+* Generate default error pages if none are provided in the configuration.
+* Be able to serve a fully static website.
+* Support file uploads.
+* Implement at least `GET`, `POST` and `DELETE` methods.
+* Keep 100% availability during stress tests with `siege`.
+* Be able to listen to multiple ports.
+* Support configuration:
+    * Choose the port and host of each server.
+    * Set up server names (or not).
+    * Set up default error pages.
+    * Limit client body size.
+    * Set up locations with one or multiple of the following rules:
+        * Define a list of allowed methods for the location.
+        * Define a HTTP redirection.
+        * Define a root directory.
+        * Define an index file.
+        * Enable/Disable directory listing.
+        * Execute CGI.
+        * Enable file upload, including target upload directory.
+* Support cookies.
+* Support session management.
+* Handle multiple CGI interpreters.
+
+## Configuration
+We chose a configuration format inspired by Nginx. [Here](assets/config/webserv.conf) is a fully-fledged configuration for running a test server, serving static files and showing execution of CGI scripts.
+
+### Rules
+The whole configuration must be encapsulated in a `webserv` block as follows:
+```
+webserv {
+    ...
+}
+```
+Inside of the block, any amount of servers can be configured: 
+```
+webserv {
+    server {
+        ...
+    }
+
+    server {
+        ...
+    }
+}
+```
+### Options
+#### Server Block 
+Each server block has multiple levels of configuration. The only required options are: 
+```
+webserv {
+    server {
+        host        localhost;          # Host, localhost or IP address.
+        port        8080; 
+    }
+}
+```
+Every other configuration option has a default value and can therefore be left out.
+
+| Option   | Description| Default Value |
+| -------- | -------- |------- |
+| `server_name` | Host name of the server | `default.com` |
+| `access_log` | Access log path | `access.log` |
+| `client_max_body_size` | Maximum request body size | `1M` |
+| `index` | Default file to serve | ` ` |
+| `root` | Root of server (all paths will be relative to this) | `/` |
+| `allowed_methods` | List of HTTP methods to allow for this server | `GET` |
+| `autoindex` | Whether to list the content of the server's root (only applicable if `index` is not set) | `false` |
+
+#### Location Block
+For more fine-grained configuration, a specific location inside a server can be configured to have different rules. The syntax is as follows:
+```
+webserv {
+    server {
+        host        localhost;
+        port        8080;
+        root        /root/;
+
+        location /data { 
+            ...
+        }
+    }
+}
+```
+The location path is relative to the server's root - above examples expanded path will be `/root/data`. Any configuration options set here will override the server's configuration for this location.
+
+| Option   | Description| Default Value |
+| -------- | -------- |------- |
+| `index` | Default file to serve | ` ` |
+| `root` | Root of server (all paths will be relative to this) | `/` |
+| `allowed_methods` | List of HTTP methods to allow for this server | `GET` |
+| `autoindex` | Whether to list the content of the server's root (only applicable if `index` is not set) | `false` |
+| `return` | Specify a redirect path and status code | `null` |
+| `alias` | Define another directory the location should map to | `null` |
+
+#### Location Block: CGI
+A location block pointing to the `/cgi-bin` location will be treated as a CGI block, which supports setting a handler (e.g., `/usr/bin/python3`) and an extension (e.g., `.py`), additionally to all other location configuration options.
+
+When receiving a request for a file ending with `extension`, the server will try to execute it with `handler`.
--- src/Config.cpp
+++ src/Config.cpp
@@ -1,289 +1,264 @@
 #include "Config.hpp"
 #include "ConfigParser.hpp"
+#include "ConfigSetters.hpp"
 #include "LocationInfo.hpp"
 #include "Server.hpp"
 #include "Utils.hpp"
-#include "ConfigSetters.hpp"
 #include <algorithm>
+#include <arpa/inet.h>
 #include <cstddef>
+#include <cstdlib>
 #include <exception>
 #include <netinet/in.h>
 #include <stdexcept>
 #include <string>
-#include <cstdlib>
 #include <sys/socket.h>
 #include <vector>
-#include <arpa/inet.h>
-#include "ConfigSetters.hpp"
 
-Config::Config(const std::string& config_path)
-{
-	ConfigParser 		parser(config_path);
+Config::Config(const std::string &config_path) {
+  ConfigParser parser(config_path);
 
-	std::map <int, _map> servers = parser.get_servers();
+  std::map<int, _map> servers = parser.get_servers();
 
-	initialize_location_setters();
-	initialize_server_setters();
-	set_servers(servers);
+  initialize_location_setters();
+  initialize_server_setters();
+  set_servers(servers);
 }
 
-void	Config::set_servers(std::map <int, _map>& raw_servers)
-{
-	Server* new_server;
+void Config::set_servers(std::map<int, _map> &raw_servers) {
+  Server *new_server;
 
-	for (std::map <int, _map>::iterator it = raw_servers.begin(); it != raw_servers.end(); it++)
-	{
-		try
-		{	
-			new_server = new Server;
+  for (std::map<int, _map>::iterator it = raw_servers.begin();
+       it != raw_servers.end(); it++) {
+    try {
+      new_server = new Server;
 
-			configure_server_names(it->second, new_server);
-			configure_port(it->second, new_server);
-			configure_host(it->second, new_server);
+      configure_server_names(it->second, new_server);
+      configure_port(it->second, new_server);
+      configure_host(it->second, new_server);
 
-			Utils::validate_required_server_values(new_server);
+      Utils::validate_required_server_values(new_server);
 
-			configure_locations(it->second, new_server);
+      configure_locations(it->second, new_server);
 
-			for (_map::iterator map_it = it->second.begin(); map_it != it->second.end(); map_it++)
-			{
-				if (_server_setters.find(map_it->first) == _server_setters.end())
-				{
-					if (Utils::valid_server_setting(map_it->first) == false)
-					{
-						Utils::config_error_on_line(
-							map_it->second.second,
-							"'" +
-							map_it->first +
-							"' is not a valid server setting."
-						);
-					}
-					continue ;
-				}
-				(_server_setters[map_it->first])(map_it->second.first, new_server);
-			}
+      for (_map::iterator map_it = it->second.begin();
+           map_it != it->second.end(); map_it++) {
+        if (_server_setters.find(map_it->first) == _server_setters.end()) {
+          if (Utils::valid_server_setting(map_it->first) == false) {
+            Utils::config_error_on_line(map_it->second.second,
+                                        "'" + map_it->first +
+                                            "' is not a valid server setting.");
+          }
+          continue;
+        }
+        (_server_setters[map_it->first])(map_it->second.first, new_server);
+      }
 
-			_servers.push_back(new_server);
-		}
-		catch (const std::exception& e)
-		{
-			abort(new_server);
-		}
-	}
+      _servers.push_back(new_server);
+    } catch (const std::exception &e) {
+      abort(new_server);
+    }
+  }
 }
 
-void	Config::abort(Server*& new_server)
-{
-	delete new_server;
+void Config::abort(Server *&new_server) {
+  delete new_server;
 
-	for (std::vector <Server *>::iterator server = _servers.begin(); server != _servers.end(); server++)
-	{
-		delete *server;
-	}
-	for (std::vector <LocationInfo *>::iterator location = _locations.begin(); location != _locations.end(); location++)
-	{
-		delete *location;
-	}
-	throw std::runtime_error("Aborting initialization, please double-check your server configuration.\n");
+  for (std::vector<Server *>::iterator server = _servers.begin();
+       server != _servers.end(); server++) {
+    delete *server;
+  }
+  for (std::vector<LocationInfo *>::iterator location = _locations.begin();
+       location != _locations.end(); location++) {
+    delete *location;
+  }
+  throw std::runtime_error("Aborting initialization, please double-check your "
+                           "server configuration.\n");
 }
 
-void	Config::add_location(LocationInfo*& new_location, Server* new_server, bool add_to_server)
-{
-	if (new_location != NULL)
-	{
-		if (new_location->get_cgi() == true)
-		{
-			new_location->set_path("/cgi-bin");
-		}
-		_locations.push_back(new_location);
-		new_location = NULL;
-	}
-	if (add_to_server == true)
-	{
-		new_server->set_locations(_locations);
-		_locations.clear();
-	}
+void Config::add_location(LocationInfo *&new_location, Server *new_server,
+                          bool add_to_server) {
+  if (new_location != NULL) {
+    if (new_location->get_cgi() == true) {
+      new_location->set_path("/cgi-bin");
+    }
+    _locations.push_back(new_location);
+    new_location = NULL;
+  }
+  if (add_to_server == true) {
+    new_server->set_locations(_locations);
+    _locations.clear();
+  }
 }
 
-Config::location_setter_map::iterator	Config::initialize_location(int line_count, const std::string& key, const std::string& name, LocationInfo*& new_location)
-{
-	if (new_location == NULL || name != new_location->get_path())
-	{
-		if (name != "/cgi-bin")
-		{
-			if (new_location != NULL)
-			{
-				_locations.push_back(new_location);
-			}
-			new_location = new LocationInfo;
-			new_location->set_path(name);
-		}
-		else 
-		{
-			return configure_cgi(line_count, key, new_location);
-		}
-	}
-	std::string current_key = key.substr(key.find_first_of(":") + 1);
-	location_setter_map::iterator setter = _location_setters.find(current_key);
+Config::location_setter_map::iterator
+Config::initialize_location(int line_count, const std::string &key,
+                            const std::string &name,
+                            LocationInfo *&new_location) {
+  if (new_location == NULL || name != new_location->get_path()) {
+    if (name != "/cgi-bin") {
+      if (new_location != NULL) {
+        _locations.push_back(new_location);
+      }
+      new_location = new LocationInfo;
+      new_location->set_path(name);
+    } else {
+      return configure_cgi(line_count, key, new_location);
+    }
+  }
+  std::string current_key = key.substr(key.find_first_of(":") + 1);
+  location_setter_map::iterator setter = _location_setters.find(current_key);
 
-	return setter;
+  return setter;
 }
 
-void	Config::configure_locations(const _map& server, Server*& new_server)
-{
-	LocationInfo*					new_location = NULL;
+void Config::configure_locations(const _map &server, Server *&new_server) {
+  LocationInfo *new_location = NULL;
 
-	for (_map::const_iterator it = server.begin(); it != server.end(); it++)
-	{
+  for (_map::const_iterator it = server.begin(); it != server.end(); it++) {
 
-		std::string name = Utils::extract_location_name(it->first);
+    std::string name = Utils::extract_location_name(it->first);
 
-		if (name.empty() == true)
-		{
-			continue ;
-		}
+    if (name.empty() == true) {
+      continue;
+    }
 
-		location_setter_map::iterator setter = initialize_location(it->second.second, it->first, name, new_location);
-		
-		if (setter != _location_setters.end())
-		{
-			try 
-			{
-				(setter->second)(it->second.first, new_location);
-			}
-			catch (const std::exception& e)
-			{
-				delete new_location;
-				Utils::config_error_on_line(it->second.second, std::string(e.what()));
-			}
-		}
-		else
-		{
-			Utils::config_error_on_line(it->second.second, "'" + it->first + "' is not a valid location setting");
-		}
-	}
-	add_location(new_location, new_server, true);
+    location_setter_map::iterator setter =
+        initialize_location(it->second.second, it->first, name, new_location);
+
+    if (setter != _location_setters.end()) {
+      try {
+        (setter->second)(it->second.first, new_location);
+      } catch (const std::exception &e) {
+        delete new_location;
+        Utils::config_error_on_line(it->second.second, std::string(e.what()));
+      }
+    } else {
+      Utils::config_error_on_line(it->second.second,
+                                  "'" + it->first +
+                                      "' is not a valid location setting");
+    }
+  }
+  add_location(new_location, new_server, true);
 }
 
-void	Config::initialize_cgi(LocationInfo*& new_location, const std::string& identifier)
-{
-	if (new_location != NULL)
-	{
-		if (identifier != new_location->get_path())
-		{
-			add_location(new_location);
-			new_location = new LocationInfo;
-		}
-	}
-	else
-	{
-		new_location = new LocationInfo;
-	}
+void Config::initialize_cgi(LocationInfo *&new_location,
+                            const std::string &identifier) {
+  if (new_location != NULL) {
+    if (identifier != new_location->get_path()) {
+      add_location(new_location);
+      new_location = new LocationInfo;
+    }
+  } else {
+    new_location = new LocationInfo;
+  }
 }
 
-Config::location_setter_map::iterator	Config::configure_cgi(int line_count, const std::string key, LocationInfo*& new_location)
-{
-	std::string cgi_identifier = Utils::extract_cgi_identifier(key);
-	if (cgi_identifier.empty() == true)
-	{
-		Utils::config_error_on_line(line_count, "missing CGI identifier in location '" + key + "', location will not be initialized");
-		return _location_setters.end();
-	}
+Config::location_setter_map::iterator
+Config::configure_cgi(int line_count, const std::string key,
+                      LocationInfo *&new_location) {
+  std::string cgi_identifier = Utils::extract_cgi_identifier(key);
+  if (cgi_identifier.empty() == true) {
+    Utils::config_error_on_line(line_count,
+                                "missing CGI identifier in location '" + key +
+                                    "', location will not be initialized");
+    return _location_setters.end();
+  }
 
-	initialize_cgi(new_location, cgi_identifier);
-	new_location->set_path(cgi_identifier);
-	new_location->set_cgi(true);
+  initialize_cgi(new_location, cgi_identifier);
+  new_location->set_path(cgi_identifier);
+  new_location->set_cgi(true);
 
-	std::string current_key = key.substr(key.find_last_of(":") + 1);
-	
-	return _location_setters.find(current_key);
+  std::string current_key = key.substr(key.find_last_of(":") + 1);
+
+  return _location_setters.find(current_key);
 }
 
-void	Config::configure_host(_map& server, Server*& new_server)
-{
-	if (server.find("host") == server.end() or server["host"].first.empty() == true)
-	{
-		Utils::config_error_on_line(-1, "missing host in server '" + new_server->get_server_name()[0] + "'.");
-	}
+void Config::configure_host(_map &server, Server *&new_server) {
+  if (server.find("host") == server.end() or
+      server["host"].first.empty() == true) {
+    Utils::config_error_on_line(-1, "missing host in server '" +
+                                        new_server->get_server_name()[0] +
+                                        "'.");
+  }
 
-	std::string host = server["host"].first[0];
+  std::string host = server["host"].first[0];
 
-	struct in_addr	ip_address;
+  struct in_addr ip_address;
 
-	if (host == "localhost")
-	{
-		host = "127.0.0.1";
-	}
-	
-	if (inet_pton(AF_INET, host.c_str(), &ip_address) != 1)
-	{
-		Utils::config_error_on_line(server["host"].second, "'" + host + "' is not a valid IPv4 address.");
-	}
+  if (host == "localhost") {
+    host = "127.0.0.1";
+  }
 
-	new_server->set_host_address(ip_address);
+  if (inet_pton(AF_INET, host.c_str(), &ip_address) != 1) {
+    Utils::config_error_on_line(server["host"].second,
+                                "'" + host + "' is not a valid IPv4 address.");
+  }
+
+  new_server->set_host_address(ip_address);
 }
 
-void	Config::configure_server_names(_map& server, Server*& new_server)
-{
-	if (server.find("server_name") == server.end() or server["server_name"].first.empty() == true)
-	{
-		server["server_name"].first.push_back("default.com");
-	}
+void Config::configure_server_names(_map &server, Server *&new_server) {
+  if (server.find("server_name") == server.end() or
+      server["server_name"].first.empty() == true) {
+    server["server_name"].first.push_back("default.com");
+  }
 
-	std::vector <std::string> new_server_names = server["server_name"].first;
+  std::vector<std::string> new_server_names = server["server_name"].first;
 
-	for (std::vector <std::string>::const_iterator it = new_server_names.begin(); it != new_server_names.end(); it++)
-	{
-		if (std::find(_server_names.begin(), _server_names.end(), *it) != _server_names.end())
-		{
-			Utils::config_error_on_line(server["server_name"].second, "on server: '" + *it + "': name already taken.");
-		}
-	}
+  for (std::vector<std::string>::const_iterator it = new_server_names.begin();
+       it != new_server_names.end(); it++) {
+    if (std::find(_server_names.begin(), _server_names.end(), *it) !=
+        _server_names.end()) {
+      Utils::config_error_on_line(server["server_name"].second,
+                                  "on server: '" + *it +
+                                      "': name already taken.");
+    }
+  }
 
-	_server_names.insert(_server_names.begin(), new_server_names.begin(), new_server_names.end());
+  _server_names.insert(_server_names.begin(), new_server_names.begin(),
+                       new_server_names.end());
 
-	new_server->set_server_name(new_server_names);
-
+  new_server->set_server_name(new_server_names);
 }
 
-void	Config::configure_port(_map& server, Server*& new_server)
-{
-	if (server.find("port") == server.end() or server["port"].first.empty() == true)
-	{
-		Utils::config_error_on_line(-1, "missing port in server '" + new_server->get_server_name()[0] + "'.");	
-	}
+void Config::configure_port(_map &server, Server *&new_server) {
+  if (server.find("port") == server.end() or
+      server["port"].first.empty() == true) {
+    Utils::config_error_on_line(-1, "missing port in server '" +
+                                        new_server->get_server_name()[0] +
+                                        "'.");
+  }
 
-	std::string port = server["port"].first[0];
+  std::string port = server["port"].first[0];
 
-	new_server->set_port(std::atoi(port.c_str()));
+  new_server->set_port(std::atoi(port.c_str()));
 }
 
-void	Config::initialize_server_setters()
-{
-	_server_setters["access_log"] = &Setters::configure_access_log;
-	_server_setters["client_max_body_size"] = &Setters::configure_client_max_body_size;
-	_server_setters["autoindex"] = &Setters::configure_autoindex;
-	_server_setters["root"] = &Setters::configure_root;
-	_server_setters["index"] = &Setters::configure_index;
-	_server_setters["error_pages"] = &Setters::add_error_page;
+void Config::initialize_server_setters() {
+  _server_setters["access_log"] = &Setters::configure_access_log;
+  _server_setters["client_max_body_size"] =
+      &Setters::configure_client_max_body_size;
+  _server_setters["autoindex"] = &Setters::configure_autoindex;
+  _server_setters["root"] = &Setters::configure_root;
+  _server_setters["index"] = &Setters::configure_index;
+  _server_setters["error_pages"] = &Setters::add_error_page;
 }
 
-void	Config::initialize_location_setters()
-{
-	_location_setters["root"] = &Setters::set_root;
-	_location_setters["allowed_methods"] = &Setters::set_allowed_methods;
-	_location_setters["return"] = &Setters::set_return;
-	_location_setters["alias"] = &Setters::set_alias;
-	_location_setters["handler"] = &Setters::set_cgi_handler;
-	_location_setters["extension"] = &Setters::set_cgi_extension;
-	_location_setters["autoindex"] = &Setters::set_autoindex;
-	_location_setters["index"] = &Setters::set_index;
+void Config::initialize_location_setters() {
+  _location_setters["root"] = &Setters::set_root;
+  _location_setters["allowed_methods"] = &Setters::set_allowed_methods;
+  _location_setters["return"] = &Setters::set_return;
+  _location_setters["alias"] = &Setters::set_alias;
+  _location_setters["handler"] = &Setters::set_cgi_handler;
+  _location_setters["extension"] = &Setters::set_cgi_extension;
+  _location_setters["autoindex"] = &Setters::set_autoindex;
+  _location_setters["index"] = &Setters::set_index;
 }
 
-Config::~Config() 
-{
-	for (std::vector <Server *>::iterator it = _servers.begin(); it != _servers.end(); it++)
-	{
-		delete *it;
-	}
+Config::~Config() {
+  for (std::vector<Server *>::iterator it = _servers.begin();
+       it != _servers.end(); it++) {
+    delete *it;
+  }
 }
